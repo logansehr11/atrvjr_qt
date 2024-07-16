f9d8b05ab743bbed0c6d3723586954aa9dfbb9b2
logansehr11 <logansehr@gmail.com>
2024-07-16 16:20:11 -0400

    Created Window with Multiple Widgets

--- src/qtInterface.cpp
+++ src/qtInterface.cpp
@@ -3,25 +3,33 @@
 #include <QCamera>
 #include <QCameraViewfinder>
 #include <QPushButton>
+#include <QWidget>
 #include "rclcpp/rclcpp.hpp"
-
-class Camera {
-    QCamera* camera;
-    QCameraViewfinder* viewfinder;
-public:
-    Camera(): camera(new QCamera), viewfinder(new QCameraViewfinder){
-        viewfinder->show();
-        camera->setViewfinder(viewfinder);
-        camera->start();
-    }
-    ~Camera(){
-        delete camera;
-        delete viewfinder;
-    }
-};
 
 int main(int argc, char* argv[]){
     QApplication app(argc, argv);
-    Camera c;
+    QWidget window;
+
+    window.resize(1000, 1000);
+    window.setWindowTitle(QApplication::translate(
+        "JuniorWindow", "Junior Window"));
+
+    QPushButton button(QApplication::translate(
+        "JuniorWindow", "Button"), &window);
+    button.resize(256, 64);
+    button.move((window.size().width() - button.size().width())/2, 900);
+    button.show();
+
+    QCamera camera;
+    QCameraViewfinder vf(&window);
+    vf.resize(800, 600);
+    vf.move((window.size().width() - vf.size().width())/2, (window.size().height() - vf.size().height())/2);
+
+    vf.show();
+    camera.setViewfinder(&vf);
+    camera.start();
+
+    window.show();
+
     return app.exec();
 }
