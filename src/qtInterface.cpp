40d4b6ad6337f6c10803f7ffddff40408c0d7c60
logansehr11 <logansehr@gmail.com>
2024-07-16 12:57:26 -0400

    Working Camera

--- src/qtInterface.cpp
+++ src/qtInterface.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
 #include <QApplication>
+#include <QCamera>
+#include <QCameraViewfinder>
 #include <QPushButton>
 #include "rclcpp/rclcpp.hpp"
 
+class Camera {
+    QCamera* camera;
+    QCameraViewfinder* viewfinder;
+public:
+    Camera(): camera(new QCamera), viewfinder(new QCameraViewfinder){
+        viewfinder->show();
+        camera->setViewfinder(viewfinder);
+        camera->start();
+    }
+    ~Camera(){
+        delete camera;
+        delete viewfinder;
+    }
+};
+
 int main(int argc, char* argv[]){
     QApplication app(argc, argv);
-
-    QPushButton button("Hello World!");
-    button.show();
-
+    Camera c;
     return app.exec();
 }
