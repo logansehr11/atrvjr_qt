d5656fc25a6cab26750fe9846c6d767c13b886d9
logansehr11 <logansehr@gmail.com>
2024-07-22 11:56:29 -0400

    Reintroduced Camera Functionality in New Interface

--- include/atrvjr_qt/main_gui.hpp
+++ include/atrvjr_qt/main_gui.hpp
@@ -4,6 +4,8 @@
 #include <QWidget>
 #include <QMainWindow>
 #include <QApplication>
+#include <QCamera>
+#include <QCameraViewfinder>
 
 #include "atrvjr_qt/button_pub.hpp"
 
@@ -15,6 +17,8 @@ class MainGUI : public QMainWindow {
     const std::shared_ptr<InterfaceNode> pubNode;
     QWidget window;
     QPushButton* button;
+    QCamera camera;
+    QCameraViewfinder* vf;
 
  public: 
     MainGUI(const std::shared_ptr<InterfaceNode>& initPubNode);
--- src/main_gui.cpp
+++ src/main_gui.cpp
@@ -1,6 +1,8 @@
 #include "atrvjr_qt/main_gui.hpp"
 
 #include <QPushButton>
+#include <QCamera>
+#include <QCameraViewfinder>
 
 MainGUI::MainGUI(const std::shared_ptr<InterfaceNode>& initPubNode) : QMainWindow(nullptr), pubNode(initPubNode) {
     window.resize(800, 664);
@@ -11,20 +13,16 @@ MainGUI::MainGUI(const std::shared_ptr<InterfaceNode>& initPubNode) : QMainWindo
     button->resize(256, 64);
     button->move((window.size().width() - button->size().width())/2, 600);
     connect(button, &QPushButton::clicked, this, &MainGUI::process_click);
+    
+    vf = new QCameraViewfinder(&window);
+    vf->resize(800, 600);
+    vf->move((window.size().width() - vf->size().width())/2, 0);
+
+    vf->show();
+    camera.setViewfinder(*(&vf));
+    camera.start();
     button->show();
     window.show();
-
-    /*
-    WORK IN PROGRESS CAMERA 
-    QCamera camera;
-    QCameraViewfinder vf(&window);
-    vf.resize(800, 600);
-    vf.move((window.size().width() - vf.size().width())/2, 0);
-
-    vf.show();
-    camera.setViewfinder(&vf);
-    camera.start();
-    */
 }
 
 MainGUI::~MainGUI() {}
